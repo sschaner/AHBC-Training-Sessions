21db9312a4095ff7014cc1effb20d04eb4e85852
Steve Schaner <schanerst@gmail.com>
2021-10-25 22:01:44 -0400

    Initial Commit

--- AHBC Day 2/Program.cs
+++ AHBC Day 2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace AHBC_Week_2
 {
@@ -6,49 +7,451 @@ namespace AHBC_Week_2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What are you doing?");
+            string answer = Console.ReadLine();
+            string outputMessage = "";
+
+            int funValue = GetWednesdayNightFun(answer, out outputMessage);
+
+            Console.WriteLine($"Fun level: {funValue}, {outputMessage}");
+            Console.ReadKey();
+
+            // DoStringManipulation();
+
+            SayHello();
+            // Get the user's name
+            string userName = GetUserName();
+            // Tell user they have a nice name
+            Console.WriteLine($"{userName} is a very nice name.");
+
+            int userAge = GetUserAge();
+
+            int computerAge = 50;
+            int ageDifference = 50;
+
+            try
+            {
+                ageDifference = CalculateUserAgeDifference(userAge, computerAge); // Must be in the same order as below
+            }
+            catch (ArgumentException ex)
+            {
+                // Handle retry logic
+
+            }
+
+
+            // Order doesn't matter if you do it this way. Not used often.
+            ageDifference = CalculateUserAgeDifference(computerAgeValue: computerAge, userAgeValue: userAge);
+
+            // Tell the user, by name, the age difference and ask if it makes sense.
+            bool doesMakeSense = AskIfMakesSense(userName, ageDifference);
+
+            // Get ticket price
+            double price = GetTotalPrice(2, true);
+
+
+
+            // Either way is fine
+            int answerFromMethod = AddNumbers();
+            int copyOfAnswer = -1;
+            copyOfAnswer = AddNumbers();
+
+            string friends = GetFriendList();
+
+            Console.ReadKey();
+        }
+
+        public static bool ValidateNumber(string userInput, int minValue, int maxValue, out int validNumber)
+        {
+            try
+            {
+                int number = int.Parse(userInput);
+                if (number >= minValue && number <= maxValue)
+                {
+                    validNumber = number;
+                    return true;
+                }
+                else
+                {
+                    validNumber = -1;
+                    return false;
+                }
+            }
+            catch (FormatException ex)
+            {
+                validNumber = -1;
+                return false;
+            }
+        }
+
+        public static void TestLogic()
+        {
+            string friendList = "matt|mary";
+            string userInput = "scott";
+            AddFriendToList(userInput, ref friendList);
+            Console.WriteLine(friendList);
+        }
+
+        public static string GetFriendList()
+        {
+            bool doContinue = true;
+            string friendList = "";
+
+            while (doContinue == true)
+            {
+                string userInput = "";
+                Console.WriteLine("What is your friends name?");
+                userInput = Console.ReadLine();
+                AddFriendToList(userInput, ref friendList);
+
+
+                Console.WriteLine("Add another friend: ");
+                userInput = Console.ReadLine();
+                if (userInput.ToLower().Trim() == "no")
+                {
+                    doContinue = false;
+                }
+            }
+
+            return friendList;
+        }
+
+        public static void GetNumberOfKids()
+        {
+            Console.WriteLine("How many kids?");
+            string userInput = Console.ReadLine();
+
+            int numberOfKids = -1;
+
+            // This is the one you'll use the most with the out parameter. Will always return a bool
+            // If true, the result is put into numberOfKids
+            bool isValidNumber = int.TryParse(userInput, out numberOfKids); // Use this for input
+
+            if (isValidNumber == false)
+            {
+                // retry logic
+            }
+        }
+
+        // ref means "reference" (this changes the value of the variable in the calling method) <-- Not used too often
+        private static void AddFriendToList(string friend, ref string friendList)
+        {
+            friendList = friendList + "|" + friend;
+        }
+
+        // numberOfKids is optional and has a default value of 0
+        public static double GetTotalPrice(int numberOfAdults, bool getsDiscount, int numberOfKids = 0)
+        {
+            double adultPrice = 5.50;
+            double kidPrice = 1.75;
+
+            double totalPrice = numberOfAdults * adultPrice + numberOfKids * kidPrice;
+
+            if (getsDiscount == true)
+            {
+                totalPrice = GetDiscountedPrice(totalPrice);
+            }
+
+            return totalPrice;
+        }
+
+        private static double GetDiscountedPrice(double currentPrice) // private because it's being used by another class
+        {
+            const double DISCOUNTPERCENTAGE = .5;
+
+            return currentPrice * DISCOUNTPERCENTAGE;
+        }
+
+        public static bool AskIfMakesSense(string userName, int ageDifference)
+        {
+            Console.WriteLine($"{userName} the age difference is {ageDifference}");
+            Console.WriteLine("Does this make sense (yes/no)?");
+            string userResponse = Console.ReadLine();
+
+            if (userResponse.ToLower().Trim() == "yes")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static int CalculateUserAgeDifference(int userAgeValue, int computerAgeValue) // Must be in the same order as above
+        {
+            if (userAgeValue <= 0 || computerAgeValue <= 0)
+            {
+                throw new ArgumentException("Ages must be greater than 0.");
+
+            }
+
+            int difference = computerAgeValue - userAgeValue;
+
+            if (userAgeValue > computerAgeValue)
+            {
+                difference = userAgeValue - computerAgeValue;
+            }
+            else
+            {
+                difference = computerAgeValue - userAgeValue;
+            }
+
+            return difference;
+        }
+
+        public static string GetUserName()
+        {
+            Console.Write("What is your name: ");
+            string userInput = Console.ReadLine();
+            return userInput;
+        }
+
+        public static int GetWednesdayNightFun(string activity, out string message)
+        {
+            int funValue = -1;
+            switch (activity.ToLower().Trim())
+            {
+                case "coding":
+                    funValue = 10;
+                    message = "You must be a coding whiz!";
+                    break;
+                case "streaming":
+                case "reading":
+                    funValue = 9;
+                    message = "That is a close second.";
+                    break;
+                case "mowing the lawn":
+                case "nascar":
+                    funValue = 4;
+                    message = "Look at that neatly cut lawn.";
+                    break;
+                default:
+                    funValue = 1;
+                    message = "That's no fun.";
+                    break;
+            }
+
+            return funValue;
+        }
+
+        public static int GetUserAge()
+        {
+            Console.WriteLine("How old are you?");
+            string userInput = Console.ReadLine();
+            int userAge = -1;
+
+            bool isValid = ValidateNumber(userInput, 5, 106, out userAge);
+
+            CalculateUserAgeDifference(userAge, 50);
+            
+            return userAge;
+        }
+
+        public static void SayHello()
+        {
+            Console.WriteLine("Hello.");
+        }
+
+        public static int AddNumbers()
+        {
+            int answer = 2 + 2;
+            string test = "";
+            // return answer; won't return
+            return answer;
+        }
+
+        public static string GetQuote(string sentence)
+        {
+            int locationOfQuote = sentence.IndexOf("\"");
+            int locationOfLastQuote = sentence.LastIndexOf("\"");
+            string actualQuote = sentence.Substring(locationOfQuote + 1, locationOfLastQuote - locationOfQuote - 1);
+            return actualQuote;
+        }
+
+        public static void DoStringManipulation() // method signature
+        {
             // String Method
 
             // ToLower - ToUpper
             // TrimStart - TrimEnd - Trim
 
-            // Replace
-            string message = "This course is okay.";
-            string newMessage = message.Replace('i', 'o');
-            newMessage = message.Replace("okay", "great");
-
-            string templateMessage = "hello [name] how are you?";
-            int hLocation = templateMessage.IndexOf('h'); // Searches for the index of 'h'. It is case sensitive. This will output 0
-            int oLocation = templateMessage.IndexOf('o'); // 4
-
-            int otherLocation = templateMessage.LastIndexOf('h'); // It'll count backwards. The first time it encounters it, going backwards. 13
-
-            int locationOfFirstSpace = templateMessage.IndexOf(' '); // 5
-            string firstWord = templateMessage.Substring(0, locationOfFirstSpace);
+            try
+            {
+                // Logic goes here
+                // Runs fine if no exception
 
 
-            Console.WriteLine("What is your name?");
-            string userInput = Console.ReadLine();
-            newMessage = templateMessage.Replace("[name]", userInput);
+                // Replace
+                string message = "This course is okay.";
+                string newMessage = message.Replace('i', 'o');
+                newMessage = message.Replace("okay", "great");
 
-            // StartWIth, EndsWith
-            bool startsWithMyLetter = newMessage.StartsWith("hel"); // true
-            bool isQuestion = newMessage.EndsWith('?');
+                string templateMessage = "hello [name] how are you?";
+                int hLocation = templateMessage.IndexOf('h'); // Searches for the index of 'h'. It is case sensitive. This will output 0
+                int oLocation = templateMessage.IndexOf('o'); // 4
 
-            // https://regexr.com/ // Can check this out but he doesn't use it.
+                int otherLocation = templateMessage.LastIndexOf('h'); // It'll count backwards. The first time it encounters it, going backwards. 13
 
-            // Split
-            string friends = "Jeff|Mary|Jacob|Sylvia";
-            string[] friendsList = friends.Split('|');
+                int locationOfFirstSpace = templateMessage.IndexOf(' '); // 5
+                string firstWord = templateMessage.Substring(0, locationOfFirstSpace);
 
-            string userRecord = "Scott|200|160|28";
-            string[] userInfo = userRecord.Split('|'); // | (pipe) is the most common to split
-            string name = userInfo[0];
-            string weight = userInfo[1];
-            string targetHeartRate = userInfo[2];
-            string bmi = userInfo[3];
+                string punctuation = templateMessage.Substring(templateMessage.Length - 1, 1); // Don't need the ,1.
+
+                string quote = "Einstein says \"e=mc2\" all the time";
+                string extractedQuote = GetQuote(quote);
+                
+
+                Console.WriteLine($"The 112th character of the quote is {quote[112]}.");
+
+                string address = @"http:\\www.here.com"; // the @ will treat the slashes as slashes
+                string filePath = @"c:\code\documents";
+
+                Console.WriteLine("What is your name?");
+                string userInput = Console.ReadLine();
+                newMessage = templateMessage.Replace("[name]", userInput);
+                bool isValidNumber = false;
+
+                while (isValidNumber == false)
+                {
+                    try
+                    {
+                        Console.WriteLine("How old are you?");
+                        userInput = Console.ReadLine();
+                        int age = int.Parse(userInput);
+                        isValidNumber = true;
+                    }
+                    catch (FormatException ex)
+                    {
+                        // Catches the exception
+                        Console.WriteLine("You need to enter an actual number.");
+                    }
+                }
 
 
-            Console.ReadKey();
+
+                int candyAmount = 12;
+                Console.WriteLine("Split the candy among your kids.");
+
+                isValidNumber = false;
+                int numberOfKids = -1;
+
+                while (isValidNumber == false)
+                {
+                    try
+                    {
+                        Console.WriteLine("How many kids do you have");
+                        userInput = Console.ReadLine();
+                        numberOfKids = int.Parse(userInput);
+                        isValidNumber = true;
+                    }
+                    catch (FormatException ex)
+                    {
+                        Console.WriteLine("You need to enter an actual number");
+                    }
+                }
+
+                try
+                {
+                    Console.WriteLine($"Each kid gets {candyAmount / numberOfKids} pieces of candy.");
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine("You don't have any kids? You get all the candy");
+                }
+
+                // StartWIth, EndsWith
+                bool startsWithMyLetter = newMessage.StartsWith("hel"); // true
+                bool isQuestion = newMessage.EndsWith('?');
+
+                // https://regexr.com/ // Can check this out but he doesn't use it.
+
+                // Split
+                string friends = "Jeff|Mary|Jacob|Sylvia";
+                string[] friendsList = friends.Split('|');
+
+                string userRecord = "Scott|200|160|28";
+                string[] userInfo = userRecord.Split('|'); // | (pipe) is the most common to split
+                string name = userInfo[0];
+                string weight = userInfo[1];
+                string targetHeartRate = userInfo[2];
+                string bmi = userInfo[3];
+
+                string firstValue = "";
+                string secondValue = null;
+
+                bool isValidString = false;
+
+                while (isValidString == false)
+                {
+                    try
+                    {
+                        Console.WriteLine("enter a value");
+                        string value = Console.ReadLine();
+
+                        // Business Logic    
+                        if (value.StartsWith('s') == false)
+                        {
+                            Console.WriteLine("string does not start with s");
+                        }
+                        isValidString = true;
+                    }
+                    catch (NullReferenceException ex)
+                    {
+                        Console.WriteLine("You need to enter an actual value");
+                    }
+                }
+
+                /*
+                if (string.IsNullOrWhiteSpace(value) == true || value.StartsWith('s') == false)
+                {
+                    Console.WriteLine("String does not start with s.");
+                }
+                */
+
+                Console.WriteLine("Do you have kids (yes/no)?");
+                userInput = Console.ReadLine();
+
+                // He uses this
+                if (userInput.Trim().ToLower() == "yes")
+                {
+
+                }
+
+                // Does the same thing as above.
+                // Use OrdinalIgnoreCase with .Equals instead of .ToLower()
+                // Developers choice!!!
+                if (string.Equals(userInput.Trim(), "yes", StringComparison.OrdinalIgnoreCase))
+                {
+
+                }
+
+                // strings are immutable - they cannot be changed
+                string myName = "Steve";
+                myName = "John";
+                myName = "Trevor";
+                // We've just created 3 names in memory
+                string realfriendList = "";
+
+                string[] realFriends = new string[] { "Jeff", "Frank", "Mary" };
+                StringBuilder builder = new StringBuilder(); // StringBuilder builds a string. In a loop as we go.
+
+                foreach (string friendName in realFriends)
+                {
+                    realfriendList = realfriendList + friendName + "|";
+                    builder.Append(friendName + "|");
+                }
+
+                // These both will print out the same thing
+                Console.WriteLine(realfriendList);
+                Console.WriteLine(builder.ToString());
+
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something unexpected happend.");
+            }
         }
     }
 }
---
+++ AHBC Week 3/Program.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Text;
+
+namespace AHBC_Week_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // LessonWork();
+            // InviteLunchBuddies();
+            // GetAverageScore();
+            // PracticeInterviewQuestions();
+
+            Random random = new Random();
+            int userInput = 12;
+            int dieOne = random.Next(1, userInput + 1);
+            int dieTwo = random.Next(1, 7);
+
+            Console.WriteLine($"Die one {dieOne}. Die two {dieTwo}.");
+
+            // 10 random numbers
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Random random = new Random();
+            //    int number = random.Next(1, 11); // Random number between 1 and 10
+            //    Console.WriteLine($"{i+1}: {number}");
+            //}
+
+            Console.ReadKey();
+        }
+
+        public static void PracticeInterviewQuestions()
+        {
+            string testString = "This is a test string";
+            //testString = "racecaR";
+            
+            char[] stringLetters = testString.ToCharArray();
+            string reversedString = "";
+            StringBuilder builder = new StringBuilder();
+
+            // Write code to display a string in reverse <-- Very common interview question for entry level developer
+            for (int i = stringLetters.Length - 1; i >= 0; i--)
+            {
+                builder.Append(stringLetters[i]);
+                //reverseString = reverseString + stringLetters[i];
+            }
+            reversedString = builder.ToString();
+            Console.WriteLine(reversedString);
+
+            // Write code to determine palidrome <-- also a common interview question
+            bool isPalidrome = false;
+
+            if (reversedString.ToLower().Trim() == testString.ToLower().Trim())
+            {
+                isPalidrome = true;
+            }
+
+            Console.WriteLine($"Is palidrome: {isPalidrome}");
+
+            // Count the number of vowels in a string
+            // Loop through characters
+            int vowelCounter = 0;
+            for (int i = 0; i < stringLetters.Length; i++)
+            {
+                switch (stringLetters[i].ToString().ToLower())
+                {
+                    case "a":
+                    case "e":
+                    case "i":
+                    case "o":
+                    case "u":
+                        // Increment vowel counter
+                        vowelCounter++;
+                        break;
+                }
+            }
+
+            string vowelsTerm = DetermineCorrectPluralUsage("vowel", vowelCounter);
+            
+
+            Console.WriteLine($"There are {vowelCounter} {vowelsTerm}.");
+
+            Console.ReadKey();
+        }
+
+        // Bring in a term and the count
+        // Return the term to use
+        
+        public static string DetermineCorrectPluralUsage(string term, int count)
+        {
+            // If count > 1 add an 's'
+            if (count > 1)
+            {
+                return term + "s";
+            }
+            else
+            {
+                return term;
+            }
+            return term;
+        }
+
+        public static void GetAverageScore()
+        {
+            int[] scores = new int[] { 98, 87, 98, 67, 56, 74, 83 };
+
+            // Add all numbers, then divide by how many scores
+            int numberOfScores = scores.Length;
+
+            int totalScore = -1;
+            for (int i = 0; i < scores.Length; i++)
+            {
+                totalScore += scores[i];
+                // totalScore = totalScore + scores[i];
+            }
+
+            double averageScore = totalScore / numberOfScores;
+            Console.WriteLine($"Your average test score is {averageScore}.");
+        }
+
+        public static void InviteLunchBuddies()
+        {
+            string[] listOfFriends = GreetZachsFriends();
+            string[] myFriends = new string[3] { "Gynell", "Hannah", "Chris" };
+
+            string[] lunchBuddies = new string[8];
+
+            listOfFriends.CopyTo(lunchBuddies, 0);
+            myFriends.CopyTo(lunchBuddies, 5);
+
+            /*
+             // Same as the CopyTo
+            int myFriendsCounter = 0;
+            for (int i = 0; i < lunchBuddies.Length; i++)
+            {
+                if (i < 5)
+                {
+                    lunchBuddies[i] = listOfFriends[i];
+                }
+
+                else
+                {
+                    lunchBuddies[i] = myFriends[myFriendsCounter];
+                    myFriendsCounter++;
+                }
+            }
+            */
+
+            for (int i = 0; i < lunchBuddies.Length; i++)
+            {
+                Console.WriteLine($"Hey {lunchBuddies[i]}, come have lunch with me.");
+            }
+        }
+
+
+
+        public static string[] GreetZachsFriends()
+        {
+            string[] friendsList = new string[] { "Brian", "Steve", "Emily", "Kyle", "Scott" };
+
+            for (int i = 0; i < friendsList.Length; i++)
+            {
+                Console.WriteLine($"Hi {friendsList[i]}!");
+            }
+
+            return friendsList;
+        }
+
+        public static void LessonWork()
+        {
+            string breakfast = "pancakes";
+
+            // declaring the array meals
+            string[] meals = new string[3];
+
+            meals[0] = "pancakes";
+            meals[1] = "sandwich";
+            meals[2] = "meatloaf";
+            // Visual code won't stop you from doing this. This is a runtime error (IndexOutOfRangeException)
+            // meals[3] = "ice cream";
+
+            int[] ages = new int[] { 12, 14, 13, 15, 20, 56 };
+
+            ages[3] = 145;
+
+            string[] names = new string[2];
+            int[] grades = new int[4];
+            bool[] answers = new bool[5];
+
+            int answerCount = answers.Length; // give the number of elements in the array 
+
+            for (int i = 0; i < answers.Length; i++)
+            {
+                Console.WriteLine($"The answer is {answers[i]}");
+            }
+
+            string[] mealNames = new string[3] { "breakfast", "lunch", "dinner" };
+
+            // This is the same as above
+            /*
+            mealNames[0] = "breakfast";
+            mealNames[1] = "lunch";
+            mealNames[2] = "dinner";
+            */
+
+            // Both the arrays have to be the same length
+            for (int i = 0; i < meals.Length; i++)
+            {
+                // Console.Write($"What was meal #{i + 1}: " );
+                Console.Write($"What did you have for {mealNames[i]}? ");
+                string userAnswer = Console.ReadLine();
+                meals[i] = userAnswer;
+            }
+
+            Console.Clear();
+            Console.WriteLine("Your meals...");
+            for (int i = 0; i < meals.Length; i++)
+            {
+                Console.WriteLine(meals[i]);
+            }
+
+            // string[] healthyMeals = new string[5];
+            string[] healthyMeals = new string[] { "bread", "milk", "butter", "soda", "cheese" };
+            // What array do you want to copy into and what element do you want to start with
+            meals.CopyTo(healthyMeals, 0); // This will override the first 3 elements.
+
+            /*
+            // This will replace the last 2 elements
+            meals.CopyTo(healthyMeals, 3);
+
+            healthyMeals[3] = "ice cream";
+            healthyMeals[4] = "pop tarts";
+            */
+
+            // This will give the default values because the arrays are empty
+            // Console.WriteLine($"name is {names[0]}, grade is {grades[0]}, answers are {answers[0]}");
+        }
+    }
+}
